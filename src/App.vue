a56d29a7641e5d87a8a93d575771c81591a8ba5e
sheaal <alisandra.sh05@gmail.com>
2024-02-21 22:33:16 +0700

    комит1

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav>
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/cart">Cart</router-link>
+      <router-link to="/order">Order</router-link>
+    </div>
+    <div>
+      <router-link to="/login">Authorization</router-link> |
+      <router-link to="/registration">Registration</router-link>
+    </div>
+  </nav>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<script setup>
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="catalog">
+    <section v-for="product in this.catalog" class="product-list">
+      <div class="product-item">
+        <h2 class="product-name">{{ product.name }}</h2>
+        <p class="product-info">{{ product.price }} руб</p>
+        <p class="product-info">{{ product.description }}</p>
+        <button class="prod-but">into a cart</button>
+      </div>
+    </section>
+  </div>
+</template>
+<style>
+.catalog{
+  gap: 30px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+.product-list{
+  background: #867a9a;
+  border-radius: 7px;
+  height: 245px;
+}
+.product-list:hover {
+  box-shadow: 2px 4px 10px #9780cb;
+}
+.prod-but{
+  border-radius: 7px;
+  background: #c9a9f3;
+}
+.prod-but:hover {
+  box-shadow: 0px 1px 13px #595858;
+}
+</style>
+<script>
+
+import axios from "axios";
+export default {
+  data() {
+    return {
+      catalog: []
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("https://jurapro.bhuser.ru/api-shop/products");
+      this.catalog = response.data.data;
+      console.log(this.catalog);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }
+};
+</script>
